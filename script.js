669b52224962690b471ee7248c8f902699dd06d6
areit <areightjust@gmail.com>
2024-10-02 20:09:42 +0300

    The number captions follow grammar rules now

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+function followGrammar(type, num) {
+    if (5 <= num && num <= 20 || num == 0) {
+        return type;
+    }
+    if (num % 10 == 1) {
+        switch(type) {
+            case "НЕДЕЛЬ": return "НЕДЕЛЯ";
+            case "ДНЕЙ": return "ДЕНЬ";
+            case "ЧАСОВ": return "ЧАС";
+            case "СЕКУНД": return "СЕКУНДА";
+            default: return type;
+        }
+    }
+    if (num % 10 < 5) {
+        switch(type) {
+            case "НЕДЕЛЬ": return "НЕДЕЛИ";
+            case "ДНЕЙ": return "ДНЯ";
+            case "ЧАСОВ": return "ЧАСА";
+            case "СЕКУНД": return "СЕКУНДЫ";
+            default: return type;
+        }
+    }
+    return type;
+}
+
 function updateCountdown() {
     const now = new Date();
     const nextSunday = new Date();
@@ -21,12 +46,11 @@ function updateCountdown() {
     const seconds = totalSeconds % 60;
     const milliseconds = Math.floor((totalMilliseconds % 1000) ); // Divide by 10 to get 2 digits
 
-    document.getElementById('weeks').textContent = `${weeks} НЕДЕЛЬ`;
-    document.getElementById('days').textContent = `${days} ДНЕЙ`;
-    document.getElementById('hours').textContent = `${hours.toString().padStart(2, '0')} ЧАСОВ`;
-    document.getElementById('minutes').textContent = `${minutes.toString().padStart(2, '0')} МИНУТ`;
-    document.getElementById('seconds').textContent = `${seconds.toString().padStart(2, '0')} СЕКУНД`;
-    document.getElementById('seconds').textContent = `${seconds.toString().padStart(2, '0')} СЕКУНД`;
+    document.getElementById('weeks').textContent = `${weeks} ${followGrammar('НЕДЕЛЬ', weeks)}`;
+    document.getElementById('days').textContent = `${days} ${followGrammar('ДНЕЙ', days)}`;
+    document.getElementById('hours').textContent = `${hours.toString().padStart(2, '0')} ${followGrammar('ЧАСОВ', hours)}`;
+    document.getElementById('minutes').textContent = `${minutes.toString().padStart(2, '0')} ${followGrammar('МИНУТ', minutes)}`;
+    document.getElementById('seconds').textContent = `${seconds.toString().padStart(2, '0')} ${followGrammar('СЕКУНД', seconds)}`;
     document.getElementById('milliseconds').textContent = `${milliseconds.toString().padStart(3, '0')} МИЛЛИСЕКУНД`;
 }
 
